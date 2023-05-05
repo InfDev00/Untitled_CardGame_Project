a8e14175303bb30aa8df306619d0d2ad68f3a2ad
InfDev00 <119485240+InfDev00@users.noreply.github.com>
2023-05-05 14:46:11 +0900

    카드 디자인 구성

---
+++ Assets/Scripts/Scripterable Object/Spell_SO.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell_SO : Card_SO //Cost,Rank, Text, Region
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
